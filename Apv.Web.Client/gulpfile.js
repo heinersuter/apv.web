993043c7ecbcecddc38bcbf31570aae5436aeb50
Heiner Suter <heiner.suter@gmx.ch>
2014-11-16 19:35:31 +0100

    Use gulp to include bower components in index.html.

--- Apv.Web.Client/gulpfile.js
+++ Apv.Web.Client/gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var plugins = gulpLoadPlugins();
+var bowerFiles = require('main-bower-files');
 
 gulp.task('default', function () {
-    // place code for your default task here
+    gulp.src('./src/index.html')
+        .pipe(plugins.inject(
+            gulp.src(bowerFiles(), { read: false }), { name: 'bower' }))
+        //.pipe(plugins.inject(
+        //    plugins.es.merge(plugins.cssFiles, gulp.src('./src/app/**/*.js', { read: false }))))
+        .pipe(gulp.dest('./src'));
 });
