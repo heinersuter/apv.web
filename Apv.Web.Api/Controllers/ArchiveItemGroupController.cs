8038eeea74be0b0f3374428fb0998d00b1bee48f
Heiner Suter <heiner.suter@gmx.ch>
2014-11-16 14:55:17 +0100

    First Controller.

---
+++ Apv.Web.Api/Controllers/ArchiveItemGroupController.cs
@@ -0,0 +1,20 @@
+﻿namespace Apv.Web.Api.Controllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+    using Models;
+
+    public class ArchiveItemGroupController : ApiController
+    {
+        private readonly ArchiveItemGroup[] _archiveItemGroups =
+        {
+            new ArchiveItemGroup {Name = "Test 1", Description = "This is my first Folder", Path = "items/2011_Pfadis_SoLa"},
+            new ArchiveItemGroup {Name = "Test 2", Description = "This is my second Folder", Path = "items/1979_Wölfe_PfiLa"},
+        };
+
+        public IEnumerable<ArchiveItemGroup> GetAllArchiveItemGroups()
+        {
+            return _archiveItemGroups;
+        }
+    }
+}
---
+++ Apv.Web.Api/Models/ArchiveItemGroup.cs
@@ -0,0 +1,10 @@
+﻿namespace Apv.Web.Api.Models
+{
+    public class ArchiveItemGroup
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
