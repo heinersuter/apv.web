408a0b68446e8b805c98a4401cfd79df240bb7a9
Heiner <heiner.suter@gmx.ch>
2014-11-24 16:58:33 +0100

    Added deploy tasks to gulp file. Not yet working.

--- Apv.Web.Client/gulpfile.js
+++ Apv.Web.Client/gulpfile.js
@@ -1,13 +1,35 @@
 var gulp = require('gulp');
 var gulpLoadPlugins = require('gulp-load-plugins');
 var plugins = gulpLoadPlugins();
-var bowerFiles = require('main-bower-files');
+var mainBowerFiles = require('main-bower-files');
+var del = require('del');
 
 gulp.task('default', function () {
     gulp.src('./src/index.html')
         .pipe(plugins.inject(
-            gulp.src(bowerFiles(), { read: false }), { name: 'bower', addPrefix: '', relative: true }))
+            gulp.src(mainBowerFiles(), { read: false }), { name: 'bower', addPrefix: '', relative: true }))
         .pipe(plugins.inject(
             gulp.src(['./src/**/*.js', './src/**/*.css'], { read: false }), { addPrefix: '', relative: true }))
         .pipe(gulp.dest('./src'));
+});
+
+gulp.task('deploy', ['deploy-clean', 'deploy-src', 'deploy-libs'], function () {
+});
+
+gulp.task('deploy-clean', function (cb) {
+    del(['./deploy/'], cb);
+});
+
+gulp.task('deploy-src', function () {
+    gulp.src(['./src/**'], { "base": "./src/" })
+        .pipe(gulp.dest('./deploy/'));
+});
+
+gulp.task('deploy-libs', ['deploy-clean'], function () {
+    var libs = gulp.src(mainBowerFiles())
+        .pipe(gulp.dest('./deploy/lib/'));
+
+    gulp.src('./src/index.html')
+        .pipe(plugins.inject(gulp.src(libs, { read: false }), { name: 'bower', addPrefix: '', relative: true }));
+
 });
