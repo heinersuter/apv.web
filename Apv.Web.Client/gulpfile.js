d2c5e8ddedaddc202afb29411596bafc9a399c6c
Heiner Suter <heiner.suter@gmx.ch>
2014-11-28 07:38:38 +0100

    Enhanced gulp deploy.

--- Apv.Web.Client/gulpfile.js
+++ Apv.Web.Client/gulpfile.js
@@ -3,33 +3,43 @@ var gulpLoadPlugins = require('gulp-load-plugins');
 var plugins = gulpLoadPlugins();
 var mainBowerFiles = require('main-bower-files');
 var del = require('del');
+var runSequence = require('run-sequence');
 
-gulp.task('default', function () {
-    gulp.src('./src/index.html')
+gulp.task('default', function (callback) {
+
+
+    return gulp.src('./deploy/index.html')
         .pipe(plugins.inject(
-            gulp.src(mainBowerFiles(), { read: false }), { name: 'bower', addPrefix: '', relative: true }))
-        .pipe(plugins.inject(
-            gulp.src(['./src/**/*.js', './src/**/*.css'], { read: false }), { addPrefix: '', relative: true }))
-        .pipe(gulp.dest('./src'));
+            gulp.src(mainBowerFiles(), { read: false })
+                .pipe(gulp.dest('./deploy/lib/'), { relative: true })))
+        .pipe(gulp.dest('./deploy/'));
+
+
+
+
+
+
+    //return runSequence('deploy-clean', 'deploy-copy', 'deploy-inject', callback);
 });
 
-gulp.task('deploy', ['deploy-clean', 'deploy-src', 'deploy-libs'], function () {
+gulp.task('deploy-clean', function (callback) {
+    return del(['./deploy/'], callback);
 });
 
-gulp.task('deploy-clean', function (cb) {
-    del(['./deploy/'], cb);
-});
-
-gulp.task('deploy-src', function () {
+gulp.task('deploy-copy', function () {
     gulp.src(['./src/**'], { "base": "./src/" })
         .pipe(gulp.dest('./deploy/'));
+
+    return gulp.src(mainBowerFiles(), { read: false })
+        .pipe(gulp.dest('./deploy/lib/'));
 });
 
-gulp.task('deploy-libs', ['deploy-clean'], function () {
-    var libs = gulp.src(mainBowerFiles())
-        .pipe(gulp.dest('./deploy/lib/'));
-
-    gulp.src('./src/index.html')
-        .pipe(plugins.inject(gulp.src(libs, { read: false }), { name: 'bower', addPrefix: '', relative: true }));
+gulp.task('deploy-inject', function () {
+    return gulp.src('./deploy/index.html')
+        .pipe(plugins.inject(
+            gulp.src(['./deploy/app/**/*.js', './deploy/app/**/*.css'], { read: false }), { addPrefix: '', relative: true }))
+        .pipe(plugins.inject(
+            gulp.src(['./deploy/lib/**/*.js', './deploy/lib/**/*.css'], { read: false }), { name: 'bower', addPrefix: '', relative: true }))
+        .pipe(gulp.dest('./deploy/'));
 
 });
