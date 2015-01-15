eaafd78bd40928fe8ca3ca495f749409297b6114
Heiner <heiner.suter@gmx.ch>
2015-01-15 17:07:25 +0100

    Bower

--- Apv.Web.Client/gulpfile.js
+++ Apv.Web.Client/gulpfile.js
@@ -1,45 +1,9 @@
-var gulp = require('gulp');
-var gulpLoadPlugins = require('gulp-load-plugins');
-var plugins = gulpLoadPlugins();
-var mainBowerFiles = require('main-bower-files');
-var del = require('del');
-var runSequence = require('run-sequence');
+var bowerFiles = require('main-bower-files'),
+    inject = require('gulp-inject'),
+    stylus = require('gulp-stylus'),
+    es = require('event-stream');
 
-gulp.task('default', function (callback) {
-
-
-    return gulp.src('./deploy/index.html')
-        .pipe(plugins.inject(
-            gulp.src(mainBowerFiles(), { read: false })
-                .pipe(gulp.dest('./deploy/lib/'), { relative: true })))
-        .pipe(gulp.dest('./deploy/'));
-
-
-
-
-
-
-    //return runSequence('deploy-clean', 'deploy-copy', 'deploy-inject', callback);
-});
-
-gulp.task('deploy-clean', function (callback) {
-    return del(['./deploy/'], callback);
-});
-
-gulp.task('deploy-copy', function () {
-    gulp.src(['./src/**'], { "base": "./src/" })
-        .pipe(gulp.dest('./deploy/'));
-
-    return gulp.src(mainBowerFiles(), { read: false })
-        .pipe(gulp.dest('./deploy/lib/'));
-});
-
-gulp.task('deploy-inject', function () {
-    return gulp.src('./deploy/index.html')
-        .pipe(plugins.inject(
-            gulp.src(['./deploy/app/**/*.js', './deploy/app/**/*.css'], { read: false }), { addPrefix: '', relative: true }))
-        .pipe(plugins.inject(
-            gulp.src(['./deploy/lib/**/*.js', './deploy/lib/**/*.css'], { read: false }), { name: 'bower', addPrefix: '', relative: true }))
-        .pipe(gulp.dest('./deploy/'));
-
-});
+gulp.src('./src/index.html')
+  .pipe(inject(gulp.src(bowerFiles(), {read: false}), {name: 'bower'}))
+  .pipe(gulp.src('./src/app/**/*.js', {read: false}))
+  .pipe(gulp.dest('./build'));
