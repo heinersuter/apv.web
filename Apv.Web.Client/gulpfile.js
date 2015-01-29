64d8e484ecb2e7428e429d73ef60f3f2a0794562
hsu <hsu@zuehlke.com>
2015-01-29 16:02:59 +0100

    FTP Gulp but not yet working.

--- Apv.Web.Client/gulpfile.js
+++ Apv.Web.Client/gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var mainBowerFiles = require('main-bower-files');
 var inject = require('gulp-inject');
 var rename = require("gulp-rename");
+var ftp = require('gulp-ftp');
 
 gulp.task('default', function () {
     gulp.src('./src/index.template.html')
@@ -9,4 +10,14 @@ gulp.task('default', function () {
         .pipe(inject(gulp.src(mainBowerFiles(), { read: false }), { name: 'bower', addPrefix: '', relative: true }))
         .pipe(inject(gulp.src(['./src/**/*.js', './src/**/*.css'], { read: false }), { addPrefix: '', relative: true }))
         .pipe(gulp.dest('./src'));
+});
+
+gulp.task('deploy', function () {
+    return gulp.src('src/*')
+            .pipe(ftp({
+                host: 'website.com',
+                user: 'johndoe',
+                pass: '1234',
+                remotePath: '/'
+            }));
 });
