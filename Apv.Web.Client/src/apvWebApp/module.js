43bf52344c035e521331e56eab4078a1c02d0666
Heiner <heiner.suter@gmx.ch>
2015-02-03 10:27:28 +0100

    Use Angular Routing. But won't run from local directory in Chrome anymore.

--- Apv.Web.Client/src/apvWebApp/module.js
+++ Apv.Web.Client/src/apvWebApp/module.js
@@ -1,1 +1,13 @@
-﻿angular.module('apvWebApp', []);
+﻿angular.module('apvWebApp', ['ngRoute']);
+
+angular.module('apvWebApp').config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/archive', {
+        templateUrl: 'apvWebApp/archive/archive.html',
+        controller: 'ArchiveCtrl'
+      }).
+      otherwise({
+        redirectTo: '/archive'
+      });
+  }]);
