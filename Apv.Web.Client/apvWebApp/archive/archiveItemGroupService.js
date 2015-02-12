ba574178e9d9a60819674838bc62767b32df5efe
Heiner <heiner.suter@gmx.ch>
2015-02-12 10:39:56 +0100

    Deploy to Hostfactory.

--- Apv.Web.Client/apvWebApp/archive/archiveItemGroupService.js
+++ Apv.Web.Client/apvWebApp/archive/archiveItemGroupService.js
@@ -1,5 +1,6 @@
 ﻿angular.module("apvWebApp").factory("ArchiveItemGroupService", function ($resource) {
     "use strict";
 
-    return $resource("http://localhost:49538/api/ArchiveItemGroup/:id/");
+    //return $resource("http://localhost:49538/api/ArchiveItemGroup/:id/");
+    return $resource("/apvwebapi/api/ArchiveItemGroup/:id/");
 });
